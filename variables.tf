318bb416cb5b74fe61d2814c6a621254cf0ede91
Yuri Cordeiro <yuricordeiroc@hotmail.com>
2024-05-14 13:39:22 -0300

    Include dafault values for non sense vars

--- variables.tf
+++ variables.tf
@@ -7,42 +7,49 @@ variable "atlas_org_id" {
 variable "atlas_project_name" {
   type        = string
   description = "Atlas Project Name"
+  default = "order-manager"
 }
 
 # Atlas Project Environment
 variable "environment" {
   type        = string
   description = "The environment to be built"
+  default = "environment"
 }
 
 # Cluster Instance Size Name 
 variable "cluster_instance_size_name" {
   type        = string
   description = "Cluster instance size name"
+  default = "M0"
 }
 
 # Cloud Provider to Host Atlas Cluster
 variable "cloud_provider" {
   type        = string
   description = "AWS or GCP or Azure"
+  default = "AWS"
 }
 
 # Atlas Region
 variable "atlas_region" {
   type        = string
   description = "Atlas region where resources will be created"
+  default = "US_EAST_1"
 }
 
 # MongoDB Version 
 variable "mongodb_version" {
   type        = string
   description = "MongoDB Version"
+  default = "6.0"
 }
 
 # IP Address Access
 variable "ip_address" {
   type = string
   description = "IP address used to access Atlas cluster"
+  default = "0.0.0.0/0"
 }
 
 # Mongodb atlas public key
@@ -73,29 +80,35 @@ variable "aws_secret_access_key" {
 variable "aws_region" {
   type = string
   description = "AWS Region"
+  default = "us-east-1"
 }
 
 variable "vpc_id" {
   type = string
   description = "AWS VPC ID"
+  default = "vpc-0a5631d68e32e6699"
 }
 
 variable "route_table_id" {
   type = string
   description = "VPC Route Table"
+  default = "rtb-013f2114158d07269"
 }
 
 variable "subnetA" {
   type = string
   description = "SubnetA ID"
+  default = "subnet-09eb19942faa0589d"
 }
 
 variable "subnetB" {
   type = string
   description = "SubnetB ID"
+  default = "subnet-0ca2c685ec3e897c3"
 }
 
 variable "subnetC" {
   type = string
   description = "SubnetC ID"
+  default = "subnet-0c9235a23c8769293"
 }
