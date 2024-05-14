83b593dfe038e92935ec0aa89a729315d8f3333e
Yuri Cordeiro <yuricordeiroc@hotmail.com>
2024-05-14 17:22:50 -0300

    Making some fixes into AtlasDB Resources and Atlas API Keys

--- main.tf
+++ main.tf
@@ -15,6 +15,10 @@ resource "mongodbatlas_database_user" "db-user" {
     database_name = "${var.atlas_project_name}"
     collection_name = "*"
   }
+  roles {
+    role_name     = "atlasAdmin"
+    database_name = "admin"
+  }
 }
 
 # Create a Database Password
@@ -27,18 +31,8 @@ resource "random_password" "db-user-password" {
 # Create Database IP Access List 
 resource "mongodbatlas_project_ip_access_list" "ip" {
   project_id = mongodbatlas_project.atlas-project.id
-  //ip_address = var.ip_address
   cidr_block = var.ip_address
 }
-
-# resource "mongodbatlas_serverless_instance" "atlas-cluster" {
-#   project_id   = mongodbatlas_project.atlas-project.id
-#   name         = "${var.atlas_project_name}-${var.environment}-cluster-serverless"
-
-#   provider_settings_backing_provider_name = var.cloud_provider
-#   provider_settings_provider_name = "SERVERLESS"
-#   provider_settings_region_name = var.atlas_region
-# }
 
 # Create Free Tier Cluster
 resource "mongodbatlas_advanced_cluster" "atlas-cluster" {
@@ -58,19 +52,3 @@ resource "mongodbatlas_advanced_cluster" "atlas-cluster" {
     }
   }
 }
-
-## Free Tier
-# Wait until VPC Creation to create Atlas Cluster
-# data "mongodbatlas_advanced_cluster" "atlas-cluser" {
-#   project_id = mongodbatlas_project.atlas-project.id
-#   name       = mongodbatlas_advanced_cluster.atlas-cluster.name
-#   depends_on = [mongodbatlas_privatelink_endpoint_service.atlaseplink]
-# }
-
-## Serverless
-# # Wait until VPC Creation to create Atlas Cluster
-# data "mongodbatlas_serverless_instance" "atlas-cluser" {
-#   project_id = mongodbatlas_project.atlas-project.id
-#   name       = mongodbatlas_serverless_instance.atlas-cluster.name
-#   depends_on = [mongodbatlas_privatelink_endpoint_service_serverless.atlaseplink]
-# }
--- output.tf
+++ output.tf
@@ -1,7 +1,4 @@
-# Outputs to Display
-// Serverless Tier
-# output "atlas_cluster_connection_string" { value = mongodbatlas_serverless_instance.atlas-cluster.connection_strings_standard_srv }
-// Free Tier
+## Outputs to Display
 output "atlas_cluster_connection_string" { value = mongodbatlas_advanced_cluster.atlas-cluster.connection_strings.0.standard_srv }
 output "ip_access_list"    { value = mongodbatlas_project_ip_access_list.ip.ip_address }
 output "project_name"      { value = mongodbatlas_project.atlas-project.name }
@@ -10,10 +7,3 @@ output "user_password"     {
   sensitive = true
   value = mongodbatlas_database_user.db-user.password 
 }
-#  output "privatelink_connection_string" {
-# #     // servless
-# #   value = mongodbatlas_serverless_instance.atlas-cluster.connection_strings_private_endpoint_srv
-#      // free tier
-#    value = mongodbatlas_advanced_cluster.atlas-cluster.connection_strings[0].private
-#    //value = mongodbatlas_advanced_cluster.atlas-cluster.connection_strings[0].private_srv
-#  }
